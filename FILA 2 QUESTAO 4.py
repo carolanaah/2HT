82bee112d5b54ccb3df1eebdb979059258956f5d
carol <134284332+carolanaah@users.noreply.github.com>
2023-05-23 08:53:35 -0300

    Add files via upload

---
+++ FILA 2 QUESTAO 4.py
@@ -0,0 +1,6 @@
+preco = float(input("Digite o valor do produto: "))
+desconto = preco * 0.10 #0.10 = 10%
+
+novo_preco = preco - desconto
+
+print("O novo valor do produto com desconto é: R$", novo_preco)
