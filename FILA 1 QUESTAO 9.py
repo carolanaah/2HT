f5c2d687f0426442d8247dd3f4f144a0ce7e7183
carol <134284332+carolanaah@users.noreply.github.com>
2023-05-23 08:26:24 -0300

    Add files via upload

---
+++ FILA 1 QUESTAO 9.py
@@ -0,0 +1,7 @@
+largura = int(input("Escreva  largura da piscina (m): "))
+comprimento = int(input("Escreva o comprimento da piscina (m)": ))
+profundidade = int(input("Escreva a profundidade  piscina (m): "))
+
+volume = largura * comprimento * profundidade
+
+print("O volume de água  piscina será:", volume em, "metros cúbicos.")
